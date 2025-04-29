f377fcb4ab0e0a46a3c3d203cefe3312962ba08d
yzfzzz <20zfye@stu.edu.cn>
2025-04-29 09:15:06 +0800

    feat(user): 实现用户注册、登录和信息获取功能
    - 新增用户注册、登录和信息获取的数据库操作函数
    - 实现用户注册、登录和信息获取的 HTTP 接口处理函数
    - 添加静态资源处理和路由配置
    - 新增通用的 HTTP 响应消息结构和工具函数

---
+++ db/user.go
@@ -0,0 +1,95 @@
+package db
+import (
+	mydb "filestore_server/db/mysql"
+	"fmt"
+)
+
+// 通过用户名及密码完成user表的注册操作
+func UserSignup(username string, password string) bool {
+	stmt,err := mydb.DBConn().Prepare("insert ignore into tbl_user (`user_name`, `user_pwd`) values (?, ?)")
+	if(err != nil){
+		fmt.Println("Failed to prepare statement, err:" + err.Error())
+		return false
+	}
+	defer stmt.Close()
+
+	ret, err := stmt.Exec(username, password)
+	if(err != nil){
+		fmt.Println("Failed to insert, err: "+err.Error())
+		return false
+	}
+	// 重复注册也算是失败
+	if rowsAffected,err := ret.RowsAffected();err== nil && rowsAffected > 0 {
+		return true
+	}
+	return false
+}
+
+// 判断密码是否匹配
+func UserSignin(username string, encpwd string) bool {
+	stmt, err := mydb.DBConn().Prepare("select * from tbl_user where user_name=? limit 1")
+	if(err != nil){
+		fmt.Println("Failed to prepare statement, err:" + err.Error())
+		return false
+	}
+	rows, err := stmt.Query(username)
+	if(err != nil){
+		fmt.Println(err.Error())
+		return false
+	}else if(rows == nil){
+		fmt.Println("username:" + username + " not exist")
+		return false
+	}
+
+	pRows := mydb.ParseRows(rows)
+	if(len(pRows)>0 && string(pRows[0]["user_pwd"].([]byte)) == encpwd){
+		return true
+	}
+	fmt.Println("user signed in failed!")
+	return false
+}
+
+// 刷新用户登录的token
+func UpdateToken(username string, token string) bool {
+	stmt, err := mydb.DBConn().Prepare("replace into tbl_user_token(`user_name`, `user_token`) values(?,?)")
+	if(err != nil){
+		fmt.Println(err.Error())
+		return false
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(username, token)
+	if(err != nil){
+		fmt.Println(err.Error())
+		return false
+	}
+	return true
+}
+
+type User struct {
+	Username string
+	Email string
+	Phone string
+	SignupAt string
+	LastActiveAt string
+	Status int
+}
+
+func GetUserInfo(username string) (User, error) {
+	user := User{}
+	stmt,err := mydb.DBConn().Prepare(
+		"select user_name, signup_at from tbl_user where user_name=? limit 1")
+	if(err != nil){
+		fmt.Println(err.Error())
+		return user, err
+	}
+	defer stmt.Close()
+
+	// 执行查询的操作
+	// Scan: 把查询结果保存到变量中
+	err = stmt.QueryRow(username).Scan(&user.Username, &user.SignupAt)
+	if(err != nil){
+		return user,err
+	}
+	return user,nil
+}
